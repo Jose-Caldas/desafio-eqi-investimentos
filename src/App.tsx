61eae0fa97acaba4140a5b7c186fb084e2a291b7
José Caldas <jrcaldas@yahoo.com.br>
2023-02-01 11:37:38 -0300

    feat: added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { Container } from "./components/Container";
 
 function App() {
   return (
     <div className="App">
       <h1>Desafio eqi investimentos</h1>
+      <Container />
     </div>
   );
 }
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Wrapper, Header, Title, Section } from "./styles";
+
+export const Container = () => {
+  return (
+    <Wrapper>
+      <Header>
+        <Title>Todo Header...</Title>
+      </Header>
+      <Section>
+        <Title>Todo Section...</Title>
+      </Section>
+    </Wrapper>
+  );
+};
