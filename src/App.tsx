63625375a14525f20492238570d0942929084130
José Caldas <jrcaldas@yahoo.com.br>
2023-02-01 19:03:19 -0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import "./App.css";
 import { Container } from "./components/Container";
 
 function App() {
   return (
     <div className="App">
-      <h1>Desafio eqi investimentos</h1>
       <Container />
     </div>
   );
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,14 +1,56 @@
 import React from "react";
-import { Wrapper, Header, Title, Section } from "./styles";
+
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
+import CloseIcon from "@material-ui/icons/Close";
+import SearchOutlinedIcon from "@material-ui/icons/Search";
+import ForwardOutlinedIcon from "@material-ui/icons/ForwardOutlined";
+
+import {
+  Wrapper,
+  Header,
+  Title,
+  Menu,
+  Section,
+  IconWrapper,
+  InputCenter,
+  SearchWrapper,
+  InputRight,
+  SectionTitle,
+  Icon,
+  Search,
+} from "./styles";
 
 export const Container = () => {
   return (
     <Wrapper>
       <Header>
-        <Title>Todo Header...</Title>
+        <Title>Calculadora de investimentos</Title>
+        <Menu>
+          <IconWrapper>
+            <Icon>
+              <ForwardOutlinedIcon />
+            </Icon>
+            <Icon>
+              <ForwardOutlinedIcon />
+            </Icon>
+            <Icon>
+              <CloseIcon />
+            </Icon>
+            <Icon>
+              <HomeOutlinedIcon />
+            </Icon>
+          </IconWrapper>
+          <InputCenter />
+          <SearchWrapper>
+            <Search>
+              <SearchOutlinedIcon />
+            </Search>
+            <InputRight />
+          </SearchWrapper>
+        </Menu>
       </Header>
       <Section>
-        <Title>Todo Section...</Title>
+        <SectionTitle>Simulador de investimentos</SectionTitle>
       </Section>
     </Wrapper>
   );
